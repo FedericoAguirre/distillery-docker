46fa73ad97c601c307f62af81b9da885bfb18cfc
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2021-09-16 18:12:00 -0500

    fastapi todo postback agregado

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,11 +1,26 @@
+import logging
+import aioredis
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings, BaseModel
+from aioredis.exceptions import ResponseError
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
+class Config(BaseSettings):
+    # The default URL expects the app to run using Docker and docker-compose.
+    redis_url: str = 'redis://redis:6379'
+    
+class Todo(BaseModel):
+    todo: str = None
+
+log = logging.getLogger(__name__)
+config = Config()
+redis = aioredis.from_url(config.redis_url, decode_responses=True)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=str(Path(BASE_DIR, "static"))), name="static")
 templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
@@ -13,3 +28,12 @@ templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("todos.html", {"request": request})
+
+@app.post("/addTodo", response_class=HTMLResponse)
+async def read_item(todo: Todo, request: Request):
+    todo_list = []
+    response = ""
+    if todo.todo != "first_todo" :
+        todo_list.append(todo.todo)
+        response = templates.TemplateResponse("todo_rows.html", {"todo_list": todo_list, "request": request})
+    return response
